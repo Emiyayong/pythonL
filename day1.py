8b21d967ed35008b807e7db55eafd4f42bd56ea3
gouyong <gouyong@gouyongdeMacBook-Air.local>
2018-05-23 00:55:04 +0800

    函数学习

--- day1.py
+++ day1.py
@@ -1,1 +1,15 @@
 print("first python progrem")
+def count():
+   def f(j):
+       def g():
+           return j*j
+       return g
+   fs=[]
+   for i in range(1,4):
+       fs.append(f(i))
+   return fs;
+f1,f2,f3=count()
+print(f1())
+print(f2())
+print(f3())
+      
